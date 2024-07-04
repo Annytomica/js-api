5edae3bb4b79493211a84e99ea9b888105d95d95
Anny Devoy <annydevoyalt@gmail.com>
2024-07-04 13:21:59 +0000

    Add function to convert options to comma seperated list for API requirements

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,25 @@ const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal')
 document.getElementById('status').addEventListener('click', e => getStatus(e));
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 
+// function to convert options (eg. ES6, ES8) into a comma seperated list as required by API
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === 'options') {   //so only create list with options not other entries in object
+            optArray.push(entry[1]); //just add the value of entry to list array
+        }
+    }
+    form.delete('options'); // deletes all the entries in data object with key of 'options'
+
+    form.append('options', optArray.join()); //adds back the new array, but as comma seperated list using .join
+
+    return form;
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById('checksform')));
 
     const response = await fetch(API_URL, {
         method: "POST",
